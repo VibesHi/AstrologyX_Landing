044f627e30709c8371815de7ef63f47207efe211
Artsiom Pryshchepau <51959359+catoha13@users.noreply.github.com>
2026-04-14 17:10:30 +0300

    new landing (#7)
    * first iteration of redesign
    
    * second iteration
    
    * third iteration
    
    * improve hero section
    
    * improve hero section
    
    * hide scroll indicator on desktop
    
    * fixes from comments in figma
    
    * add video preview of the app
    
    * add adaptive layout, fix mobile layout
    
    * add image
    
    * fix appstore banner
    
    * fix
    
    * add chevron to scroll down
    
    * remove section
    
    * fix texts
    
    * improve terms and privacy
    
    * add corner for video
    
    * fixes

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+export const APP_STORE_URL = 'https://apps.apple.com/app/id1449148271';
+export const APP_STORE_ID = '1449148271';
+export const APP_NAME = 'AstrologyX';
+/** Shown on the landing hero next to the App Store badge */
+export const APP_VERSION = '4.0.2';
